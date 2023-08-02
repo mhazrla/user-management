8011b3faf595e8b8a022774a8ef7ce52f4a5947d
mhazrla <mhazrla@gmail.com>
2023-08-03 01:34:28 +0700

    first fcommit =

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Super Admin',
+            'username' => 'superadmin',
+            'email' => 'superadmin@gmail.com',
+            'address' => 'address',
+            'birth_date' => 'birth_date',
+            'phone_number' => 'phone_number',
+            'photo' => 'photo',
+            'password' => bcrypt('123123123')
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Create New User</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('users.index') }}"> Back </a>
+            </div>
+        </div>
+    </div>
+
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong>Something went wrong.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+
+    {!! Form::open(['route' => 'users.store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                {!! Form::text('name', null, ['placeholder' => 'Name', 'class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Username:</strong>
+                {!! Form::text('username', null, ['placeholder' => 'Username', 'class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Phone Number:</strong>
+                {!! Form::text('phone_number', null, ['placeholder' => 'Phone Number', 'class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Birth date:</strong>
+                {!! Form::text('birth_date', null, ['placeholder' => 'Birth date', 'class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Photo:</strong>
+                {!! Form::file('photo', null, ['placeholder' => 'Photo', 'class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Address:</strong>
+                {!! Form::text('address', null, ['placeholder' => 'Address', 'class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Email:</strong>
+                {!! Form::text('email', null, ['placeholder' => 'Email', 'class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Password:</strong>
+                {!! Form::password('password', ['placeholder' => 'Password', 'class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Confirm Password:</strong>
+                {!! Form::password('confirm-password', ['placeholder' => 'Confirm Password', 'class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Role:</strong>
+                {!! Form::select('roles[]', $roles, [], ['class' => 'form-control', 'multiple']) !!}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+    {!! Form::close() !!}
+
+@endsection
